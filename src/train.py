9217bd661c1c322f85d9e3876cf699272a7d1c21
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2021-10-18 18:10:36 +0000

    dvc: commit experiment ef306a8b5694ac59bd6262c049caf7d033cdfa4fb5a35b45bc7491234243accb

--- src/train.py
+++ src/train.py
@@ -4,6 +4,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Conv2D, Flatten, MaxPooling2D
 from tensorflow.keras.utils import to_categorical
 
+from dvclive.keras import DvcLiveCallback
 OUTPUT_DIR = "output"
 fpath = os.path.join(OUTPUT_DIR, "data.pkl")
 with open(fpath, "rb") as fd:
@@ -46,28 +47,6 @@ model.compile(loss='categorical_crossentropy',
               optimizer=opt,
               metrics=['accuracy'])
 
-history = model.fit(x_train, y_train, epochs=num_epochs, batch_size=batch_size, verbose=1)
-
-def history_to_csv(history):
-    # This code is copied from https://github.com/iterative/get-started-experiments/
-    keys = list(history.history.keys())
-    csv_string = ",".join(["epoch"] + keys) + "\n"
-    list_len = len(history.history[keys[0]])
-    for i in range(list_len):
-        row = (
-            str(i + 1)
-            + ","
-            + ",".join([str(history.history[k][i]) for k in keys])
-            + "\n"
-        )
-        csv_string += row
-
-    return csv_string
-print(history)
-with open("output/train_logs.csv", "w") as f:
-    f.write(history_to_csv(history))
-
-model.summary()
-
-model_file = os.path.join(OUTPUT_DIR, "model.h5")
-model.save(model_file)
+model.fit(
+    x_train, y_train, epochs=num_epochs, batch_size=batch_size, verbose=1,
+    callbacks=[DvcLiveCallback(os.path.join(OUTPUT_DIR, "model.h5"))])
